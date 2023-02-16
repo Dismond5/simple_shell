11b1793e92b3d980c5aad62eda90f1d3521df62c
dismond5 <omarkhaled201148@gmail.com>
2023-02-16 08:43:37 -0800

    a

---
+++ string.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: input
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * _strcmp - performs lexicogarphic comparison
+ * @s1: input
+ * @s2: input
+ * Return:negative, positive or 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * starts_with - checks if needle starts with haystack
+ * @haystack: input
+ * @needle: input
+ * Return: add
+ */
+
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input
+ * @src: input
+ * Return: pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
